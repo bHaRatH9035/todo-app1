2fb70975755b8e4ed2dfbab5d79e9164d737492a
b0m0ebv <b0m0ebv@homeoffice.wal-mart.com>
2025-02-10 18:17:07 +0530

    initial commit

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value || '';
+  
+  if (!token) {
+    console.log('token not found ', token);
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+  return NextResponse.rewrite(new URL('/', request.url));
+}
+
+export const config = {
+  matcher: [
+    '/'
+  ]
+}
