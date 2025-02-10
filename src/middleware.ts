611cf2b93ca0d0cfbcc244e8645dc4a72e14fe7a
b0m0ebv <b0m0ebv@homeoffice.wal-mart.com>
2025-02-10 18:37:12 +0530

    adding more logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value || '';
   
   if (!token) {
-    console.log('token not found ', token);
+    console.log('token not found ', token, process.env);
     return NextResponse.redirect(new URL('/login', request.url));
   }
   return NextResponse.rewrite(new URL('/', request.url));
